4b55992b68d3ebd5071b02718a678f30f9897f3a
Stephen Brown <stephen.jr36@gmail.com>
2024-08-30 10:00:24 -0600

    completed up to problem 4

--- answers.sql
+++ answers.sql
@@ -12,3 +12,15 @@ WHERE customer_id = (
 );
 
 -- Problem 3
+SELECT SUM(num_cupcakes)
+FROM orders
+WHERE processed = 'f';
+
+-- Problem 4
+SELECT c.name, SUM(o.num_cupcakes)
+FROM cupcakes AS c
+LEFT JOIN orders AS o
+  ON c.id = o.cupcake_id
+GROUP BY c.name
+ORDER BY c.name;
+
